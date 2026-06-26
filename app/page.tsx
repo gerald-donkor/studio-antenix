de5af70d820080fe2679d060b80da7047ae3da4f
gerald-donkor <donkorgerald0@gmail.com>
2026-06-26 11:27:46 +0000

    04: Implement About

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Hero from "@/components/hero"
+import About from '@/components/about'
 
 export default function Home() {
   return (
     <main>
       <Hero />
+      <About />
     </main>
   )
 }
